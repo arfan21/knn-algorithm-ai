55bcb8b00714aa429a10089279464ceb596fc064
arfan21 <arfan.jadul10@gmail.com>
2021-06-26 16:48:55 +0700

    update hanya pake supremum

--- knn.py
+++ knn.py
@@ -1,14 +1,13 @@
 import pandas as pd
-import numpy as np
 import json
 
 
 class KNN():    # Membuat class KNN yang berisi method untuk menghitung jarak, mendapatkan data terbaik, dan print data
-    def __init__(self, data, testData): #Method untuk menginisialisasi isi dari class KNN
+    def __init__(self, data, testData):  # Method untuk menginisialisasi isi dari class KNN
         self.data = data
         self.testData = testData
 
-    def euclidean(self):    #Method untuk menghitung jarak Euclidean
+    def euclidean(self):  # Method untuk menghitung jarak Euclidean
         self.resultEuclidean = []
         for _, series in self.data.iterrows():
             resultRow = 0
@@ -23,7 +22,7 @@ class KNN():    # Membuat class KNN yang berisi method untuk menghitung jarak, m
                 {"Nama Mobil": series["Nama Mobil"], "Result": resultRow**(1/2)})
         return self.resultEuclidean
 
-    def manhattan(self):    #Method untuk menghitung jarak Manhattan
+    def manhattan(self):  # Method untuk menghitung jarak Manhattan
         self.resultManhattan = []
         for _, series in self.data.iterrows():
             resultRow = 0
@@ -38,7 +37,7 @@ class KNN():    # Membuat class KNN yang berisi method untuk menghitung jarak, m
                 {"Nama Mobil": series["Nama Mobil"], "Result": resultRow})
         return self.resultManhattan
 
-    def minkowski(self, h=3):    #Method untuk menghitung jarak Minkowski dengan h default adalah 3
+    def minkowski(self, h=3):  # Method untuk menghitung jarak Minkowski dengan h default adalah 3
         self.resultMinkowski = []
         for _, series in self.data.iterrows():
             resultRow = 0
@@ -53,7 +52,7 @@ class KNN():    # Membuat class KNN yang berisi method untuk menghitung jarak, m
                 {"Nama Mobil": series["Nama Mobil"], "Result": resultRow**(h)})
         return self.resultMinkowski
 
-    def supremum(self):    #Method untuk menghitung jarak Supremum
+    def supremum(self):  # Method untuk menghitung jarak Supremum
         self.resultSupremum = []
         for _, series in self.data.iterrows():
             resultRow = []
@@ -72,55 +71,64 @@ class KNN():    # Membuat class KNN yang berisi method untuk menghitung jarak, m
         """Get all distance"""
 
         return {
-            "Euclidean": self.euclidean(),  #Mengembalikan jarak euclidean
-            "Manhattan": self.manhattan(),  #Mengembalikan jarak manhattan
-            "Minkowski": self.minkowski(),  #Mengembalikan jarak minkowski
-            "Supremum": self.supremum(),    #Mengembalikan jarak supremum
+            "Euclidean": self.euclidean(),  # Mengembalikan jarak euclidean
+            "Manhattan": self.manhattan(),  # Mengembalikan jarak manhattan
+            "Minkowski": self.minkowski(),  # Mengembalikan jarak minkowski
+            "Supremum": self.supremum(),  # Mengembalikan jarak supremum
         }
 
-    def printDistance(self):    #Method untuk print semua jarak
-        """Pretty Print all distance"""
+    def printDistance(self, k=3):  # Method untuk print data terbaik dari semua metode distance
+        """Pretty Print best data from all distance"""
 
-        print(json.dumps(self.distances(), sort_keys=False, indent=4))
+        allDistance = self.distances()
 
-    def getBestData(self, k=3): #Method untuk mendapatkan data terbaik dari setiap metode perhitungan jarak dengan k default adalah 3
-        """Mendapatkan data terbaik dari setiap metode perhitungan jarak"""
-        
-        #Melakukan sorting terhadap data jarak secara ascending
-        bestEuclidean = sorted(self.euclidean(), key=lambda x: x["Result"])[:k] 
-        bestManhattan = sorted(self.manhattan(), key=lambda x: x["Result"])[:k]
-        bestMinkowski = sorted(self.minkowski(), key=lambda x: x["Result"])[:k]
-        bestSupremum = sorted(self.supremum(), key=lambda x: x["Result"])[:k]
+        # Melakukan sorting terhadap data jarak secara ascending
+        bestEuclidean = sorted(
+            allDistance["Euclidean"], key=lambda x: x["Result"])[:k]
+        bestManhattan = sorted(
+            allDistance["Manhattan"], key=lambda x: x["Result"])[:k]
+        bestMinkowski = sorted(
+            allDistance["Minkowski"], key=lambda x: x["Result"])[:k]
+        bestSupremum = sorted(
+            allDistance["Supremum"], key=lambda x: x["Result"])[:k]
 
-        #Menambahkan data terbaik kedalam satu array data terbaik
-        data = []
-        for i in range(k):
-            data.append(
-                {"Method": "Euclidean", "Nama Mobil": bestEuclidean[i]["Nama Mobil"], "Result": bestEuclidean[i]["Result"]})
-        for i in range(k):
-            data.append(
-                {"Method": "Manhattan", "Nama Mobil": bestManhattan[i]["Nama Mobil"], "Result": bestManhattan[i]["Result"]})
-        for i in range(k):
-            data.append(
-                {"Method": "Minkowski", "Nama Mobil": bestMinkowski[i]["Nama Mobil"], "Result": bestMinkowski[i]["Result"]})
-        for i in range(k):
-            data.append(
-                {"Method": "Supremum", "Nama Mobil": bestSupremum[i]["Nama Mobil"], "Result": bestSupremum[i]["Result"]})
+        data = {
+            "Euclidean": bestEuclidean,
+            "Manhattan": bestManhattan,
+            "Minkowski": bestMinkowski,
+            "Supremum": bestSupremum,
+        }
 
-        return data
+        print(json.dumps(data, sort_keys=False, indent=4))
 
-    def printBestData(self):    #Method untuk print data terbaik
+    # Method untuk mendapatkan data terbaik dari metode perhitungan jarak supremum dengan k default adalah 3
+    def getBestData(self, k=3):
+        """Mendapatkan data terbaik dari metode perhitungan jarak supremum"""
+
+        allDistance = self.distances()
+
+        # Melakukan sorting terhadap data jarak secara ascending
+        bestSupremum = sorted(
+            allDistance["Supremum"], key=lambda x: x["Result"])[:k]
+
+        bestData = []   # Array yang menyimpan nama mobil terbaik
+        for i in range(len(bestSupremum)):
+            bestData.append(bestSupremum[i]["Nama Mobil"])
+
+        return bestData
+
+    def printBestData(self):  # Method untuk print data terbaik
         """Pretty Print best data each method"""
 
         print(json.dumps(self.getBestData(), sort_keys=False, indent=4))
 
 
-def createTestData():   #Fungsi untuk membuat data tes
-    ukuran = 1
-    kenyamanan = 1
-    irit = 1
-    kecepatan = 1
-    harga = 1
+def createTestData():  # Fungsi untuk membuat data tes
+    ukuran = 7
+    kenyamanan = 7
+    irit = 7
+    kecepatan = 7
+    harga = 2.3
 
     data = {'Ukuran': [ukuran], 'Kenyamanan': [kenyamanan], 'Irit': [irit], 'Kecepatan': [kecepatan],
             'Harga (Ratus Juta)': [harga]}
@@ -128,7 +136,8 @@ def createTestData():   #Fungsi untuk membuat data tes
     return pd.DataFrame(data)
 
 
-def normalisasi(dataframe, maxRange=10):    #Fungsi untuk melakukan normalisasi terhadap data training
+# Fungsi untuk melakukan normalisasi terhadap data training
+def normalisasi(dataframe, maxRange=10):
     coppiedDataframe = dataframe.copy()
     for col in coppiedDataframe.columns:
         if not coppiedDataframe[col].dtype == "int64" and not coppiedDataframe[col].dtype == "float64":
@@ -140,13 +149,23 @@ def normalisasi(dataframe, maxRange=10):    #Fungsi untuk melakukan normalisasi 
     return coppiedDataframe
 
 
-data = pd.read_excel("./mobil.xls") #Membaca file excel mobil.xls
+def main():
+    data = pd.read_excel("./mobil.xls")  # Membaca file excel mobil.xls
 
-normalizedData = normalisasi(data)  #Melakukan normalisasi data training dari mobil.xls
-testData = createTestData()         #Membuat data tes
-knn = KNN(normalizedData, testData) #Melakukan inisialisasi class knn
-knn.getBestData()                   #Mendapatkan data terbaik
-knn.printBestData()                 #Print data terbaik
+    # Melakukan normalisasi data training dari mobil.xls
+    normalizedData = normalisasi(data)
+    print(normalizedData, "\n")
+    testData = createTestData()  # Membuat data tes
+    knn = KNN(normalizedData, testData)  # Melakukan inisialisasi class knn
+    bestData = knn.getBestData()  # Mendapatkan data terbaik
+    knn.printDistance()
+    knn.printBestData()  # Print data terbaik
 
-recommendedData = pd.DataFrame(knn.getBestData())   #Menyimpan data terbaik pada dataframe
-recommendedData.to_excel('rekomendasi.xlsx', index=False)   #Menyimpan dataframe data terbaik menjadi file rekomendasi.xlsx
+    # Menyimpan data terbaik pada dataframe
+    recommendedData = pd.DataFrame(bestData)
+    # Menyimpan dataframe data terbaik menjadi file rekomendasi.xlsx
+    recommendedData.to_excel('rekomendasi.xlsx', index=False)
+
+
+if __name__ == '__main__':
+    main()
